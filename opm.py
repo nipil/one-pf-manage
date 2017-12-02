29f79a799aea1bf9d74d2f3270ce27754de99fdd
nipil <nipil@users.noreply.github.com>
2017-12-02 10:28:28 +0100

    basic template

---
+++ opm.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+
+
+class App:
+
+    def __init__(self, args):
+        self.args = args
+        self.setup_logging()
+
+    def setup_logging(self):
+        # root logger
+        numeric_level = getattr(logging, self.args.log_level.upper())
+        self.root_logger = logging.getLogger()
+        self.root_logger.setLevel(numeric_level)
+        # format
+        log_format = "%(message)s"
+        if numeric_level == logging.DEBUG:
+            log_format = " ".join([
+                "thread=%(threadName)s",
+                "module=%(module)s",
+                "func=%(funcName)s",
+                "line=%(lineno)d",
+                ": {0}"]).format(log_format)
+        # logging output
+        handler = logging.StreamHandler()
+        log_format = "%(asctime)s %(levelname)s {0}".format(log_format)
+        # finalize
+        formatter = logging.Formatter(log_format)
+        handler.setFormatter(formatter)
+        self.root_logger.addHandler(handler)
+        logging.debug("Command line arguments: {0}".format(args))
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    import argparse
+    import sys
+
+    try:
+        parser = argparse.ArgumentParser(description="one-pf-manage")
+        parser.add_argument("-l",
+                            "--log-level",
+                            metavar="LVL",
+                            choices=[
+                                "critical",
+                                "error",
+                                "warning",
+                                "info",
+                                "debug"],
+                            default="warning")
+        parser.add_argument("action",
+                            choices=[
+                                "create",
+                                "update",
+                                "destroy"])
+        parser.add_argument("jsonfile")
+
+        args = parser.parse_args()
+
+        app = App(args)
+        app.run()
+
+        sys.exit(0)
+
+    except KeyboardInterrupt as e:
+        logging.warning("Caught SIGINT (Ctrl-C), exiting.")
+        sys.exit(1)
+
+    except SystemExit as e:
+        message = "Exiting with return code {0}".format(e.code)
+        if e.code == 0:
+            logging.info(message)
+        else:
+            logging.warn(message)
+            raise e
+
+    except Exception as e:
+        logging.critical("{0}: {1}".format(e.__class__.__name__, e))
+        # when debugging, we want the stack-trace
+        if args.log_level == "debug":
+            raise e
